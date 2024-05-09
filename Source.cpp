709d06f457928e4fce641101251339fee8722ea9
quexzz <kind654@ya.ru>
2024-05-09 23:24:23 +0400

    Программа исправлена так, что последовательность чисел начинается со значений (0, 1, ...)

--- Source.cpp
+++ Source.cpp
@@ -3,14 +3,13 @@
 using namespace std;
 
 int fib(int n) {
-    if (n <= 1) {
-        return n;
-    }
-    else {
+    if (n == 0)
+        return 1;
+    else if (n == 1)
+        return 0;
+    else
         return fib(n - 1) + fib(n - 2);
-    }
 }
-
 
 int main() {
     cout << "Hello, World!" << endl;
